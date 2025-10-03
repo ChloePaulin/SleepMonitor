074cbf73e512c0c2ab5700f0981488925a38ba52
Chloé PAULIN <chloe.previl@gmail.com>
2025-10-03 12:28:40 +0200

    Add RectangleForm in widgets/styles for all pages + add logo

--- app/src/main/java/fr/simsa/sleepmonitor/MainActivity.kt
+++ app/src/main/java/fr/simsa/sleepmonitor/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import fr.simsa.sleepmonitor.ui.theme.SleepMonitorTheme
+import fr.simsa.sleepmonitor.widgets.styles.RectangleForm
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +23,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             SleepMonitorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column {
+                        RectangleForm(
+                            modifier = Modifier.fillMaxWidth(),
+                        ) {
+                            Row(
+                                Modifier.padding(innerPadding)
+                            ) {
+                                Text("J'ai rajouté du texte")
+                            }
+
+                        }
+                    }
                 }
             }
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    SleepMonitorTheme {
-//        Greeting("Android")
-//    }
-//}
