be4fa11590be3d105513ff2f4547f8ee302715aa
Chloé PAULIN <chloe.previl@gmail.com>
2025-10-06 16:26:16 +0200

    add background color on profile

--- app/src/main/java/fr/simsa/sleepmonitor/widgets/views/Profile.kt
+++ app/src/main/java/fr/simsa/sleepmonitor/widgets/views/Profile.kt
@@ -1,7 +1,22 @@
 package fr.simsa.sleepmonitor.widgets.views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import fr.simsa.sleepmonitor.ui.theme.BlueLightPolice
 
-fun Profile(modifier: Modifier = Modifier){
-
+@Composable
+fun Profile(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .background(
+                color = BlueLightPolice
+            )
+            .fillMaxSize()
+    ) {
+        Text("Je suis ton profil")
+    }
 }
